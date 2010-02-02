11057389f73552b383514874da54c80171a083a4
Joe Gooch <mrwizard@k12system.com>
2010-02-02 12:10:24 -0500

    Track whether a service entry is global or not

--- pound.h
+++ pound.h
@@ -344,6 +344,7 @@ typedef struct _service {
     LHASH               *sessions;  /* currently active sessions */
     int                 dynscale;   /* true if the back-ends should be dynamically rescaled */
     int                 disabled;   /* true if the service is disabled */
+    int			global;     /* true if this service is global rather than attached to a specific listener */
     struct _service     *next;
 }   SERVICE;
 
