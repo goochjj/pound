97278aa35637d4545edb3efb8d981849a42c0d4e
Joe Gooch <mrwizard@k12system.com>
2010-08-10 17:58:06 -0400

    Update debian changelog for package version

--- generate_version.sh
+++ generate_version.sh
@@ -8,4 +8,14 @@
 #: Nothing
 pwd
 git describe --tags --long | sed -e 's,^\(.*\)$,char *POUND_VERSION = "\1";,' > version.c
-
+git checkout debian/changelog
+(
+  DV=`git describe --tags --long |sed -e 's,^v\([0-9]*\.[0-9]*\)\.[0-9]*-\([0-9]*\).*,\1-\2,'`
+  echo "sapphire-pound25 ($DV) unstable; urgency=high"
+  echo ""
+  echo "  * Update package version number to source control"
+  echo ""
+  echo " -- Joseph Gooch <mrwizard@k12system.com>  "$(date -R)
+  echo ""
+  cat debian/changelog
+) > hi && mv -f hi debian/changelog
