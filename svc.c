190178917116c0b2774254d2d85a0f17a366233f
Joe Gooch <mrwizard@k12system.com>
2010-09-08 11:54:22 -0400

    fix rewrite - address/port match with correct protocol should immediately return 0

--- svc.c
+++ svc.c
@@ -1116,11 +1116,16 @@ need_rewrite(const int rewr_loc, char *const location, char *const path, const L
            * check if the Location points to the Listener.... Ports must match, if protocol is wrong, rewrite.
            */
           if(memcmp(&be_addr.sin_addr.s_addr, &in_addr.sin_addr.s_addr, sizeof(in_addr.sin_addr.s_addr)) == 0
-          && memcmp(&be_addr.sin_port, &in_addr.sin_port, sizeof(in_addr.sin_port)) == 0
-          && strcasecmp(proto, (lstn_chk->ctx == NULL)? "http": "https") ) {
-              if (logredirects) logmsg(LOG_DEBUG, "REDIR: global listener matched" );
-              free(addr.ai_addr);
-              return (lstn_chk->ctx==NULL)?2:3;
+          && memcmp(&be_addr.sin_port, &in_addr.sin_port, sizeof(in_addr.sin_port)) == 0) {
+              if (strcasecmp(proto, (lstn_chk->ctx == NULL)? "http": "https") == 0) {
+                if (logredirects) logmsg(LOG_DEBUG, "REDIR: global listener matched with correct protocol, REDIR is correct" );
+                free(addr.ai_addr);
+                return 0;
+              } else {
+                if (logredirects) logmsg(LOG_DEBUG, "REDIR: global listener matched with incorrect protocol, needs rewrite." );
+                free(addr.ai_addr);
+                return (lstn_chk->ctx==NULL)?2:3;
+              }
           }
         } else {
           memcpy(&be6_addr, lstn_chk->addr.ai_addr, sizeof(be6_addr));
@@ -1128,11 +1133,16 @@ need_rewrite(const int rewr_loc, char *const location, char *const path, const L
            * check if the Location points to the Listener.... Ports must match, protocol will be rewritten
            */
           if(memcmp(&be6_addr.sin6_addr.s6_addr, &in6_addr.sin6_addr.s6_addr, sizeof(in6_addr.sin6_addr.s6_addr)) == 0
-          && memcmp(&be6_addr.sin6_port, &in6_addr.sin6_port, sizeof(in6_addr.sin6_port)) == 0
-          && strcasecmp(proto, (lstn_chk->ctx == NULL)? "http": "https") ) {
-              if (logredirects) logmsg(LOG_DEBUG, "REDIR: global listener matched" );
-              free(addr.ai_addr);
-              return (lstn_chk->ctx==NULL)?2:3;
+          && memcmp(&be6_addr.sin6_port, &in6_addr.sin6_port, sizeof(in6_addr.sin6_port)) == 0) {
+              if (strcasecmp(proto, (lstn_chk->ctx == NULL)? "http": "https") == 0) {
+                if (logredirects) logmsg(LOG_DEBUG, "REDIR: global listener matched with correct protocol, REDIR is correct" );
+                free(addr.ai_addr);
+                return 0;
+              } else {
+                if (logredirects) logmsg(LOG_DEBUG, "REDIR: global listener matched with incorrect protocol, needs rewrite" );
+                free(addr.ai_addr);
+                return (lstn_chk->ctx==NULL)?2:3;
+              }
           }
         }
       }
