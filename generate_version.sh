504cf33daabc918f1de28ca802effc6e6f96bb1e
Joe Gooch <mrwizard@k12system.com>
2010-09-27 16:02:06 -0400

    first stab at fixing pound compile, static/shared libs,match upstream/v tags, pull full 3 octet version if available
    set package sections
    -Bstatic magic
    generate version when running git-buildpackage, debian branch

--- generate_version.sh
+++ generate_version.sh
@@ -7,10 +7,12 @@
 
 #: Nothing
 pwd
-git describe --tags --long | sed -e 's,^\(.*\)$,char *POUND_VERSION = "\1";,' > version.c
+if [ ! -z "$GBP_GIT_DIR" ]; then export GIT_DIR="$GBP_GIT_DIR"; fi
+#if [ ! -z "$GBP_BUILD_DIR" ]; then cd "$GBP_BUILD_DIR" || exit 1; fi
+git describe --tags --long --match upstream/v* | sed -e 's,^\(upstream/\|\)\(.*\)$,char *POUND_VERSION = "\2";,' > version.c
 git checkout debian/changelog
 (
-  DV=`git describe --tags --long |sed -e 's,^v\([0-9]*\.[0-9]*\)\.[0-9]*-\([0-9]*\).*,\1-\2,'`
+  DV=`git describe --tags --long --match upstream/v* |sed -e 's,^\(upstream/\|\)v\([0-9]*\.[\.0-9]*\)-\([0-9]*\).*,\2-\3,'`
   echo "sapphire-pound25 ($DV) unstable; urgency=high"
   echo ""
   echo "  * Update package version number to source control"
