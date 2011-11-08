852ad185f8fce953082418cf30d21543e3269612
Joseph Gooch <mrwizard@k12system.com>
2011-11-08 15:31:28 -0500

    generate version when running git-buildpackage

--- generate_version.sh
+++ generate_version.sh
@@ -7,6 +7,8 @@
 
 #: Nothing
 pwd
+if [ ! -z "$GBP_GIT_DIR"]; then export GIT_DIR="$GBP_GIT_DIR"; fi
+#if [ ! -z "$GBP_BUILD_DIR" ]; then cd "$GBP_BUILD_DIR" || exit 1; fi
 git describe --tags --long --match upstream/v* | sed -e 's,^\(upstream/\|\)\(.*\)$,char *POUND_VERSION = "\2";,' > version.c
 git checkout debian/changelog
 (
