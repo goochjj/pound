aa420385d02713014279aef81a23f211d945706a
Joe Gooch <mrwizard@k12system.com>
2012-03-19 10:58:34 -0400

    ThreadModel implementation
    Implement a config option for pool (2.6 behavior) vs dynamic (2.5 behavior)

--- pound.h
+++ pound.h
@@ -272,6 +272,8 @@ extern char *user,              /* user to run as */
 extern long ctrl_mode;          /* octal mode of the control socket */
 
 extern int  numthreads,         /* number of worker threads */
+            threadpool,         /* 1 to use a threadpool (i.e. 2.6 behavior)
+                                   0 to use new thread per request (2.5 behavior) */
             alive_to,           /* check interval for resurrection */
             daemonize,          /* run as daemon */
             log_facility,       /* log facility to use */
