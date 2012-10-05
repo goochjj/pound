0b31a8b293dccf700d90946fe71d62891eaf0aa5
Joe Gooch <mrwizard@k12system.com>
2012-10-05 11:41:48 -0400

    SSL Compression Disable patch for 2.6f
    This patch disables SSL/TLS compression entirely.  There is no config option.
    
    This prevents CRIME attacks against SSL.  Note that HTTP compression is still
    an option.
    
    Test your server at https://www.ssllabs.com/ssldb/
    
    Original patch by Hereward Cooper <coops@fawk.eu>
    Openssl 0.9.8 disabling ideas borrowed from Igor Sysoev's code in nginx.

--- config.c
+++ config.c
@@ -293,6 +293,9 @@ parse_be(const int is_emergency)
             SSL_CTX_set_verify(res->ctx, SSL_VERIFY_NONE, NULL);
             SSL_CTX_set_mode(res->ctx, SSL_MODE_AUTO_RETRY);
             SSL_CTX_set_options(res->ctx, SSL_OP_ALL);
+#ifdef SSL_OP_NO_COMPRESSION
+            SSL_CTX_set_options(res->ctx, SSL_OP_NO_COMPRESSION);
+#endif
             SSL_CTX_clear_options(res->ctx, SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION);
             SSL_CTX_clear_options(res->ctx, SSL_OP_LEGACY_SERVER_CONNECT);
             sprintf(lin, "%d-Pound-%ld", getpid(), random());
@@ -313,6 +316,9 @@ parse_be(const int is_emergency)
             SSL_CTX_set_verify(res->ctx, SSL_VERIFY_NONE, NULL);
             SSL_CTX_set_mode(res->ctx, SSL_MODE_AUTO_RETRY);
             SSL_CTX_set_options(res->ctx, SSL_OP_ALL);
+#ifdef SSL_OP_NO_COMPRESSION
+            SSL_CTX_set_options(res->ctx, SSL_OP_NO_COMPRESSION);
+#endif
             SSL_CTX_clear_options(res->ctx, SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION);
             SSL_CTX_clear_options(res->ctx, SSL_OP_LEGACY_SERVER_CONNECT);
             sprintf(lin, "%d-Pound-%ld", getpid(), random());
@@ -842,6 +848,9 @@ parse_HTTPS(void)
     POUND_CTX   *pc;
 
     ssl_op_enable = SSL_OP_ALL;
+#ifdef SSL_OP_NO_COMPRESSION
+    ssl_op_enable |= SSL_OP_NO_COMPRESSION;
+#endif
     ssl_op_disable = SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION | SSL_OP_LEGACY_SERVER_CONNECT;
 
     if((res = (LISTENER *)malloc(sizeof(LISTENER))) == NULL)
